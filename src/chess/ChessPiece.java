8a66436519c0d92e986e08b55585a02b4655e139
Gustavo Lopes <gustaa.lopes@gmail.com>
2023-10-18 10:55:22 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -14,9 +14,14 @@ public abstract class ChessPiece extends Piece{
 		this.color = color;
 	}
 
+	
 
 	public Color getColor() {
 		return color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 
 	protected boolean isThereOpponentPiece(Position position) {
